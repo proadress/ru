ab2f42de2c29d47778ddb0b74168d8b43e9d3585
cloud <yc359032@gmail.com>
2024-01-31 19:26:01 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Router};
 
 async fn hello_world() -> &'static str {
-    "Hello, world!"
+    "Hello, world!auhwrgiuhawirughuiwrhgiuhwugiwrhgiuawhguiawhigh"
 }
 
 #[shuttle_runtime::main]
